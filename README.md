c0ae50502ef3d0db0496516887686b463393986a
Samuel Sanchez <a348437@uach.mx>
2023-11-18 22:01:37 -0600

    servidor

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mi primer servidor HTTP
+
+Este es un ejemplo de un servidor HTTP escrito para NodeJS para la clase de Desarrollo basado en plataformas.
+
+## Autores
+
+Samuel Sánchez Tarango (Taimander)[https://github.com/Taimander]
---
+++ WWW/js/main.js
@@ -0,0 +1,10 @@
+console.log("Hola JavaScript del lado del cliente.");
+
+let button = document.getElementById("boton-ocultar");
+
+function hideImage() {
+    let image = document.getElementById("logo");
+    image.style = "display: none;";
+}
+
+button.onclick = hideImage;
